c699473c6a8a27b9f93a85fa41069c80257df3f8
Komuching <lopringjunior@gmail.com>
2024-11-14 10:16:27 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,31 +1,29 @@
 #!/bin/bash
 
+# Memastikan skrip dijalankan dengan hak akses sudo
+if [ "$EUID" -ne 0 ]; then 
+  echo "Please run as root or use sudo"
+  exit
+fi
+
 # Update package list dan upgrade semua paket
 sudo apt update && apt install sudo && sudo apt upgrade -y
 
-# Install curl
+# Install paket umum
 sudo apt install -y curl
-
-# Install wget
 sudo apt install -y wget
-
-# Install git
 sudo apt install -y git
-
-# Install unzip
 sudo apt install -y unzip
-
-# Install build-essential
 sudo apt install -y build-essential
-
-# Install software-properties-common
 sudo apt install -y software-properties-common
-
-# Install net-tools
 sudo apt install -y net-tools
-
-# Install htop
 sudo apt install -y htop
+sudo apt install -y nano vim
+sudo apt install -y ca-certificates
+sudo apt install -y tmux
+sudo apt install -y jq
+sudo apt install -y zip
+sudo apt install -y tree
 
 # Install UFW firewall dengan kebijakan default allow incoming dan outgoing
 sudo apt install -y ufw
@@ -34,25 +32,7 @@ sudo ufw default allow outgoing
 sudo ufw allow OpenSSH
 sudo ufw --force enable
 
-# Install nano dan vim
-sudo apt install -y nano vim
-
-# Install ca-certificates
-sudo apt install -y ca-certificates
-
-# Install tmux
-sudo apt install -y tmux
-
-# Install jq
-sudo apt install -y jq
-
-# Install zip
-sudo apt install -y zip
-
-# Install tree
-sudo apt install -y tree
-
-# Install OpenSSH Server
+# Install dan konfigurasi OpenSSH Server
 sudo apt install -y openssh-server
 sudo systemctl enable ssh
 
@@ -65,13 +45,9 @@ sudo npm install -g npm@latest
 sudo npm install -g pm2
 pm2 startup systemd
 
-# Install Yarn
+# Install Yarn, nodemon, dan TypeScript
 sudo npm install -g yarn
-
-# Install nodemon
 sudo npm install -g nodemon
-
-# Install TypeScript
 sudo npm install -g typescript
 
 # Install library tambahan yang diperlukan
@@ -93,12 +69,6 @@ sudo apt install -y libcurl4-openssl-dev # Untuk mendukung cURL dengan SSL
 # Bersihkan cache paket
 sudo apt autoremove -y
 sudo apt clean
-
-# Memastikan skrip dijalankan dengan hak akses sudo
-if [ "$EUID" -ne 0 ]; then 
-  echo "Please run as root or use sudo"
-  exit
-fi
 
 # Mengedit /etc/systemd/logind.conf
 LOGIND_CONF="/etc/systemd/logind.conf"
